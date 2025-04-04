a771667e970a92cadb6c3c3245c4ff4f9aa778e6
S BOOPATHY <95556016+BoopathyEngineer@users.noreply.github.com>
2025-04-04 21:03:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ print("Hello, World!")
 @app.get("/")
 def read_root():
     return {"message": "The GitHub Action ran successfully, and CI/CD has been implemented."}
+
+@app.get("/api")
+def read_root():
+    return {"message": "The jenkins ran successfully, and CI/CD has been implemented."}
